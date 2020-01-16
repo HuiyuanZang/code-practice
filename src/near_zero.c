ac989a5fb3b6dfa3e3f42f04ded028768b0e3995
courage714 <frederic.zang@gmail.com>
2020-01-16 16:45:34 +0800

    update

---
+++ src/near_zero.c
@@ -0,0 +1,20 @@
+/*
+ * Given an array of intergers, try to find two elements such that their sum is closest to zero
+ * The fisrt step, we sort array, then the issue become how to find the two element such that their sum is closest to zero O(nlgn)
+ * check arr[0]>= 0 and arr[n-1]<=0
+ * othewise,find the location m where arr[m]<0 and arr[m+1]>=0 via binary search O(lgn)
+ * find the location 
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+}
