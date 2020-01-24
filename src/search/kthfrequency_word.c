035fda02a5554622ddb05a6c846c0f980c45f05a
courage714 <frederic.zang@gmail.com>
2020-01-24 13:40:21 +0800

    update

--- src/search/kthfrequency_word.c
+++ src/search/kthfrequency_word.c
@@ -8,20 +8,20 @@
 #define TO_LOWER(c)      \
         ((c) >= 'A' && (c) <= 'Z')?(c) + 'a' -'A':(c)
 
-#define CHAR_TO_IDEX(c) (int)(TO_LOWER(c) - 'a')
+#define CHAR_TO_INDEX(c) (int)(TO_LOWER(c) - 'a')
 
 
 
 typedef enum TrieNodeType_E{
-    Trie_END;
-    Trie_NOEND;
+    Trie_END,
+    Trie_NOEND
 }TireNodeType_e;
 
 typedef struct TrieNode_T{
     TireNodeType_e type; //if no this type, have to check if all children are NULL or not 
     unsigned frequency;
     int index2heap;
-    struct TriedNode_T *child[MAX_CHARS]; //array of pointer to trie node NOTE: struct TriedNode_t
+    struct TrieNode_T *child[MAX_CHARS]; //array of pointer to trie node NOTE: struct TriedNode_t
 }TrieNode_t;
 
 typedef struct MinHeapNode_T{
@@ -38,10 +38,10 @@ typedef struct MinHeap_T{
 }MinHeap_t;
 
 
-static char tolower(char c)
+static int tolower(int c)
 {
     if (c >= 'A' && c <= 'Z')
-        return a + 'a' - 'A';
+        return (c + 'a' - 'A');
     else 
         return c;
 }
@@ -61,12 +61,12 @@ static void swap(MinHeapNode_t *node1,int index1, MinHeapNode_t *node2,int index
     tmp = *node1;
     *node1 = *node2;
     *node2 = tmp;
-    node1->node.index2heap = index2;
-    node2->node.index2heap = index1;
+    node1->node->index2heap = index2;
+    node2->node->index2heap = index1;
 }
 static TrieNode_t *create_trienode()
 {
-    TrieNode_t node = (TriedNode_t *) malloc(sizeof(TrieNode_t));
+    TrieNode_t *node = (TrieNode_t *) malloc(sizeof(TrieNode_t));
     if(node)
     {
         node->type = Trie_NOEND;
@@ -102,12 +102,12 @@ static void heapify(MinHeap_t *heap, int index)
     int left  = 2*index + 1;
     int right = 2*index + 2;
     //case 1, if node[index] hasn't left and right child
-    if ( left > size-1 )
+    if ( left > heap->size - 1 )
     {
         return;
     }
     //case 2, if node[index] has left child but no right child
-    if ( left == size )
+    if ( left == heap->size )
     {
         //if node[index] > heap->nodes[left], swap
         if(less_than(&heap->nodes[left],&heap->nodes[index]) > 0)
@@ -120,21 +120,63 @@ static void heapify(MinHeap_t *heap, int index)
     //compare node[left] and node[right] first, find the smaller child,
     //then compare with node[index]
     tmp = less_than(&heap->nodes[left],&heap->nodes[right])>0?left:right;
-    if ( less_than(heap->nodes[tmp],heap->nodes[index]) > 0 )
+    if ( less_than(&heap->nodes[tmp],&heap->nodes[index]) > 0 )
     {
-        swap(heap->nodes+tmp,tmp,heap->nodes+index,index);
+        swap(&heap->nodes[tmp],tmp,&heap->nodes[index],index);
         heapify(heap,tmp);
     }
 } 
-
-static MinHeapNode_t *addWord_Heap(MinHeap_t *heap, TriedNode_t *node, const char *word)
+static void heapify_upward(MinHeap_t *heap, int index)
 {
+    int parent = (index-1)/2;
+    while ( parent >= 0 )
+    {
+        if( less_than(&heap->nodes[index], &heap->nodes[parent]) > 0)
+        {
+            swap(&heap->nodes[index],index,&heap->nodes[parent],parent);
+            parent = (parent-1)/2;
+        }
+        else 
+            break;
+    }
+}
+
+
+static void addTrieNode_Heap(MinHeap_t *heap, TrieNode_t *node, const char *word)
+{
+    //Trie node hasn't been in heap
     if (node->index2heap == -1)
     {
+        //the heap is full and the frequency of word is more than the root of heap
+        //the root should be the kth most frequency word
+        //we need replace root with new word
+        if (heap->size == heap->capacity)
+        {
+            //before any assign new one, release old resource,
+            //detach original trie node with heap
+            heap->nodes[0].node->index2heap = -1;
+            free(heap->nodes[0].word);
+            //
+            heap->nodes[0].frequency = node->frequency;
+            heap->nodes[0].node = node;
+            heap->nodes[0].word = strdup(word);
+            node->index2heap = 0;
+
+            heapify(heap,0);
+        }
+        else//else,add new node to the latest postion
+        {
+            heap->nodes[heap->size].frequency = node->frequency;
+            heap->nodes[heap->size].word = strdup(word);
+            heap->nodes[heap->size].node = node;
+            node->index2heap = heap->size;
+            heap->size++;
+            heapify_upward(heap,heap->size - 1 );
+        }
     }
     else//Trie node already in heap
     {
-        heap->nodes[node->index2heap]->frequency++;
+        heap->nodes[node->index2heap].frequency++;
         heapify(heap,node->index2heap);
     }
 }
@@ -164,6 +206,19 @@ static TrieNode_t *addword_Trie(TrieNode_t *root, const char *word)
     return node;
 }
 
+static void add(MinHeap_t *heap, TrieNode_t *root, const char *word)
+{
+    TrieNode_t *node = addword_Trie(root, word);
+    addTrieNode_Heap(heap,node,word);
+}
+
+static void dump_heap(MinHeap_t *heap)
+{
+    for (int i=0; i<heap->size; i++)
+    {
+        printf("%s:[%d]\n", heap->nodes[i].word, heap->nodes[i].frequency);
+    }
+}
 
 
 int main(int argc, char *argv[])
